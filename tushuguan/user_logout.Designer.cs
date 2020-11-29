898b495ac6ba5ccc9c2bb76355ad064088c3f2b9
TSingSea <59871143+ssooenftzero@users.noreply.github.com>
2020-11-29 22:47:26 +0800

    logout index+完整
    logout index+完整

--- tushuguan/user_logout.Designer.cs
+++ tushuguan/user_logout.Designer.cs
@@ -40,9 +40,10 @@ namespace tushuguan
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(229, 111);
+            this.label1.Location = new System.Drawing.Point(172, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 28);
+            this.label1.Size = new System.Drawing.Size(54, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "姓名";
             // 
@@ -50,49 +51,54 @@ namespace tushuguan
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(229, 182);
+            this.label2.Location = new System.Drawing.Point(172, 146);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 28);
+            this.label2.Size = new System.Drawing.Size(54, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "学号";
             // 
             // txt_del_sno
             // 
-            this.txt_del_sno.Location = new System.Drawing.Point(319, 182);
+            this.txt_del_sno.Location = new System.Drawing.Point(239, 146);
+            this.txt_del_sno.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_del_sno.Name = "txt_del_sno";
-            this.txt_del_sno.Size = new System.Drawing.Size(263, 25);
+            this.txt_del_sno.Size = new System.Drawing.Size(198, 21);
             this.txt_del_sno.TabIndex = 2;
             this.txt_del_sno.TextChanged += new System.EventHandler(this.txt_del_sno_TextChanged);
             // 
             // txt_del_sname
             // 
-            this.txt_del_sname.Location = new System.Drawing.Point(319, 111);
+            this.txt_del_sname.Location = new System.Drawing.Point(239, 89);
+            this.txt_del_sname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_del_sname.Name = "txt_del_sname";
-            this.txt_del_sname.Size = new System.Drawing.Size(263, 25);
-            this.txt_del_sname.TabIndex = 3;
+            this.txt_del_sname.Size = new System.Drawing.Size(198, 21);
+            this.txt_del_sname.TabIndex = 1;
             this.txt_del_sname.TextChanged += new System.EventHandler(this.txt_del_sname_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(390, 287);
+            this.button1.Location = new System.Drawing.Point(292, 230);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(192, 65);
-            this.button1.TabIndex = 4;
+            this.button1.Size = new System.Drawing.Size(144, 52);
+            this.button1.TabIndex = 3;
             this.button1.Text = "用户注销";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // user_logout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txt_del_sname);
             this.Controls.Add(this.txt_del_sno);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "user_logout";
             this.Text = "user_logout";
             this.ResumeLayout(false);
